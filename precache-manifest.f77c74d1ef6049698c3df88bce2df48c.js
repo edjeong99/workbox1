0895809f12f78c251cba33ef812dd6031a206a36
Edward J <edjeong99@gmail.com>
2020-06-25 18:18:02 -0500

    Updates

---
+++ precache-manifest.f77c74d1ef6049698c3df88bce2df48c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1f120f95b959d1b993454c6cd525aef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db6296480aace3ec8ba7",
+    "url": "/static/css/main.b9de7936.chunk.css"
+  },
+  {
+    "revision": "0e82ccdfc4a531d0dc19",
+    "url": "/static/js/2.a3aa432b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a3aa432b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db6296480aace3ec8ba7",
+    "url": "/static/js/main.679d3ff2.chunk.js"
+  },
+  {
+    "revision": "d7e845661518608cb1d4",
+    "url": "/static/js/runtime-main.8bb9eb76.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
